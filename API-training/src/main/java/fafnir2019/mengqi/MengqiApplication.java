a118aebaf618db568b45471ea5201f65f9bc55be
Mengqi29 <43154273+Mengqi29@users.noreply.github.com>
2019-07-20 00:57:20 -0400

    feat: add register function

---
+++ API-training/src/main/java/fafnir2019/mengqi/MengqiApplication.java
@@ -0,0 +1,13 @@
+package fafnir2019.mengqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MengqiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MengqiApplication.class, args);
+    }
+
+}
